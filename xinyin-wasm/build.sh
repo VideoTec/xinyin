75e8f41aa4ee65acc56533ccf48d59f0434ead39
code <code@mac>
2025-09-13 22:45:40 +0700

    update pkg version

--- xinyin-wasm/build.sh
+++ xinyin-wasm/build.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-wasm-pack build --no-pack --no-typescript --out-name xinyinWasm -t web
+wasm-pack build --no-pack --out-name xinyin-wasm -t web
 
 # 将生成的 wasm 和 js 文件复制到指定目录
 OUT_DIR=../../../web/xinyin-web/src/xinyin
-cp ./pkg/xinyinWasm_bg.wasm "$OUT_DIR/xinyinWasm.wasm"
-cp ./pkg/xinyinWasm_bg.wasm "$OUT_DIR/../../public/xinyin/xinyinWasm.wasm"
-cp ./pkg/xinyinWasm.js "$OUT_DIR/xinyinWasm.js"
+cp ./pkg/xinyin-wasm_bg.wasm "$OUT_DIR/../../public/xinyin-wasm.wasm"
+cp ./pkg/xinyin-wasm_bg.wasm.d.ts "$OUT_DIR/xinyin-wasm.wasm.d.ts"
+cp ./pkg/xinyin-wasm.js "$OUT_DIR/xinyin-wasm.js"
+cp ./pkg/xinyin-wasm.d.ts "$OUT_DIR/xinyin-wasm.d.ts"
 
 # 先在文件顶部插入 @ts-nocheck
 # 在 macOS 的 BSD sed 中使用 -i '' 方式进行 inline 编辑
-sed -i '' '1s/^/\/\/ @ts-nocheck\n/' "$OUT_DIR/xinyinWasm.js"
+sed -i '' '1s/^/\/\/ @ts-nocheck\n/' "$OUT_DIR/xinyin-wasm.js"
